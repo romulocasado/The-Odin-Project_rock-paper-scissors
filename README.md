bbe501a990a2e705a459c34da53612f245183ea4
Rômulo Cavalcanti <romuloscac92@gmail.com>
2023-06-27 14:44:20 -0300

    prepare the files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # The-Odin-Project_rock-paper-scissors
+
  Rework of the first JS project from The Odin Project
+
+ After sometime focusing on life's demands, it's time to return to my programming studies, I'm redoing from scratch a project I did months ago back when I was learning HTML, CSS and JS.
+
+ I plan to re-familiarize myself with programming and finish the Odin Project Fundamentals course before the next steps.
+
+ The NUMBER ONE OBJECTIVE is to write code in a human readable way (as clear as possible), and to preserve what's considered good practice.
---
+++ script.js
@@ -0,0 +1,30 @@
+/**
+**This is the Rock Paper, Scissors console game script code
+
+TODO  
+*? PART 1
+
+*TODO a function calling the game
+
+    The function must be called playRound and take 2 other functions as parameters (playerSelection, computerSelection)
+
+*TODO a function asking for the player's choice
+
+    The function must be called getPlayerChoice
+
+
+*TODO a function generating the computer's choice
+
+    The function must be called getComputerChoice
+
+*!_______________________________________________________________________
+
+TODO
+*? PART 2
+
+*TODO add a Graphical User Interface with buttons and text
+*/
+
+
+
+
