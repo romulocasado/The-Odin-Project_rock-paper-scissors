7f8abd42ff4885355b4ab6435978e87cd2fe75bc
Rômulo Cavalcanti <romuloscac92@gmail.com>
2023-06-27 18:22:40 -0300

    sets the functions for player, computer and for one Round

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
 /**
-**This is the Rock Paper, Scissors console game script code
+**This is the Rock, Paper and Scissors console game script code, in JS
 
-TODO  
-*? PART 1
-
-*TODO a function calling the game
-
-    The function must be called playRound and take 2 other functions as parameters (playerSelection, computerSelection)
-
-*TODO a function asking for the player's choice
-
-    The function must be called getPlayerChoice
-
+TODO
 
 *TODO a function generating the computer's choice
 
     The function must be called getComputerChoice
 
-*!_______________________________________________________________________
+*TODO a function asking for the player's choice
 
-TODO
-*? PART 2
+    The function must be called getPlayerChoice
 
-*TODO add a Graphical User Interface with buttons and text
+*TODO a function calling the round
+
+    The function must be called playRound and take 2 other functions as parameters (playerSelection, computerSelection)
+
+*TODO a function calling the full 5 rounds game
+
+    The function must be called playGame and take the playRound function as a parameter
 */
 
+const gameOptions = [`rock`, `paper`, `scissors`];
 
+function getComputerChoice() {
+  let inFunctionComputerSelection =
+    gameOptions[Math.floor(gameOptions.length * Math.random())];
 
+  return inFunctionComputerSelection;
+} /**_______________________end of the function getComputerChoice()_______________________**/
 
+function getPlayerChoice() {
+  let inFunctionPlayerSelection = prompt(`write "ROCK", "PAPER" or "SCISSORS"`);
+
+  if (
+    inFunctionPlayerSelection === null ||
+    inFunctionPlayerSelection === undefined ||
+    !gameOptions.includes(inFunctionPlayerSelection.toLowerCase())
+    // checks if the prompt contains one of the 3 values provided at gameOptions array
+  ) {
+    alert(`You have to write one of the options: ROCK, PAPER, or SCISSORS`);
+    return getPlayerChoice();
+  }
+
+  inFunctionPlayerSelection = inFunctionPlayerSelection.toLowerCase();
+
+  return inFunctionPlayerSelection;
+} /**_______________________end of the function getPlayerChoice()_______________________**/
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return console.log(`It's a tie!`);
+  } else if (playerSelection === `rock` && computerSelection === `paper`) {
+    return console.log(`You Lose! Paper beats Rock!`);
+  } else if (playerSelection === `rock` && computerSelection === `scissors`) {
+    return console.log(`You Won! Rock beats Scissors`);
+  } else if (playerSelection === `paper` && computerSelection === `rock`) {
+    return console.log(`You Won! Paper beats Rock!`);
+  } else if (playerSelection === `paper` && computerSelection === `scissors`) {
+    return console.log(`You Lose! Scissors beats Paper!`);
+  } else if (playerSelection === `scissors` && computerSelection === `rock`) {
+    return console.log(`You Lose! Rock beats Scissors!`);
+  } else if (playerSelection === `scissors` && computerSelection === `paper`) {
+    return console.log(`You Won! Scissors beats Paper!`);
+  }
+} /**_______________________end of the function getPlayerChoice()_______________________**/
