9e94bc54bece4fdffe598ee8fd5df1490452d20b
Rômulo Cavalcanti <romuloscac92@gmail.com>
2023-07-12 16:18:34 -0300

    refactor: refactors script2

--- script2.js
+++ script2.js
@@ -11,14 +11,16 @@ function getComputerChoice() {
 
 function showOptions() {
   let rockLost = document.createElement("i");
-  rockLost.className = "fa-sharp fa-regular fa-hand-back-fist rock-choice";
+  rockLost.className = "fa-sharp fa-regular fa-hand-back-fist";
+  rockLost.id = "rock-choice";
 
   let paperLost = document.createElement("i");
-  paperLost.className = "fa-sharp fa-regular fa-hand paper-choice";
+  paperLost.className = "fa-sharp fa-regular fa-hand";
+  paperLost.id = "paper-choice";
 
   let scissorsLost = document.createElement("i");
-  scissorsLost.className =
-    "fa-sharp fa-regular fa-hand-scissors scissors-choice";
+  scissorsLost.className = "fa-sharp fa-regular fa-hand-scissors";
+  scissorsLost.id = "scissors-choice";
 
   const choiceContainer = document.createElement("div");
   choiceContainer.id = "choice-div";
@@ -28,33 +30,27 @@ function showOptions() {
   choiceContainer.appendChild(paperLost);
   choiceContainer.appendChild(scissorsLost);
   document.body.appendChild(choiceContainer);
+
+  getPlayerChoice();
 } /* end of the function showOptions */
 
 function getPlayerChoice() {
-  let playerSelection;
-  let clickRock = document.querySelector(".rock-choice");
-  let clickPaper = document.querySelector(".paper-choice");
-  let clickScissors = document.querySelector(".scissors-choice");
+  let playerChoice = ``;
+  let clickRock = document.getElementById("rock-choice");
+  let clickPaper = document.getElementById("paper-choice");
+  let clickScissors = document.getElementById("scissors-choice");
 
   clickRock.addEventListener("click", () => {
-    playerSelection = `rock`;
+    playerChoice = `rock`;
   });
 
   clickPaper.addEventListener("click", () => {
-    playerSelection = `paper`;
+    playerChoice = `paper`;
   });
 
   clickScissors.addEventListener("click", () => {
-    playerSelection = `scissors`;
+    playerChoice = `scissors`;
   });
-
-  if (clickRock) {
-    return (playerSelection = `rock`);
-  } else if (clickPaper) {
-    return (playerSelection = `paper`);
-  } else if (clickScissors) {
-    return (playerSelection = `scissors`);
-  }
 } /* end of the function getPlayerChoice() */
 
 function playRound() {
@@ -64,8 +60,10 @@ function playRound() {
   let versusIcon = document.createElement("i");
   versusIcon.className = "fa-solid fa-x";
 
-  let loadingIcon = document.createElement("i");
-  loadingIcon.className = "fa-regular fa-loader fa-spin";
+  let waitingIcon = document.createElement("i");
+  waitingIcon.className = "fa-solid fa-question fa-spin";
+  let waitingIcon2 = document.createElement("i");
+  waitingIcon2.className = "fa-solid fa-question fa-spin";
 
   let rockWins = document.createElement("i");
   rockWins.className = "fa-sharp fa-regular fa-hand-back-fist fa-beat-fade";
@@ -78,26 +76,17 @@ function playRound() {
 
   const matchContainer = document.createElement("div");
   matchContainer.id = "match-div";
+  document.body.appendChild(matchContainer);
+  matchContainer.textContent = `RESULTS`;
+  matchContainer.appendChild(lineBreak);
+  matchContainer.appendChild(waitingIcon);
+  matchContainer.appendChild(versusIcon);
+  matchContainer.appendChild(waitingIcon2);
 
-  if (playerSelection === computerSelection) {
-  }
-}
-/*
-  if (playerSelection === computerSelection) {
-    roundResults.push("Tie");
-    console.log(`Round ${round}: It's a tie!`);
-  } else if (
-    (playerSelection === `rock` && computerSelection === `scissors`) ||
-    (playerSelection === `paper` && computerSelection === `rock`) ||
-    (playerSelection === `scissors` && computerSelection === `paper`)
-  ) {
-    roundResults.push(`Player`);
-    console.log(`Round ${round}: You won this round!`);
-  } else {
-    roundResults.push(`Computer`);
-    console.log(`Round ${round}: You lost this round!`);
-  }
-} /*end of the function playRound()*/
+  matchScenarios();
+} /* end of the function playRound() */
+
+function matchScenarios() {} /* end of the function matchScenarios() */
 
 showOptions();
 getPlayerChoice();
